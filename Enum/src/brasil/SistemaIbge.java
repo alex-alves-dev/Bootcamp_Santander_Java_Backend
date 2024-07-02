55db7b502dcd28262e429fe42452835faf7abedc
Alex da Silva Alves <embaixadorbr@gmail.com>
2024-07-02 19:37:14 -0300

    Finalizando aula sobre Enum

--- Enum/src/brasil/SistemaIbge.java
+++ Enum/src/brasil/SistemaIbge.java
@@ -5,5 +5,11 @@ public class SistemaIbge {
 		for(EstadoBrasileiro e: EstadoBrasileiro.values()) {
 			System.out.println(e.getSigla() + "-" + e.getNome());
 		}
+		EstadoBrasileiro eb = EstadoBrasileiro.CEARA;
+		
+		System.out.println(eb.getNome());
+		System.out.println(eb.getSigla());
+		System.out.println(eb.getNomeMaiusculo());
+		System.out.println(eb.getIbge());
 	}
 }
