e60dfd98cd64c53d75dd5d28cddc713031816c3e
Alex da Silva Alves <embaixadorbr@gmail.com>
2024-07-02 19:20:57 -0300

    Commit trabalhando no SO Windows

--- Enum/src/brasil/EstadoBrasileiro.java
+++ Enum/src/brasil/EstadoBrasileiro.java
@@ -4,7 +4,9 @@ public enum EstadoBrasileiro {
 	SAO_PAULO ("SP", "São Paulo"),
 	RIO_JANEIRO ("RJ", "Rio de Janeiro"),
 	PIAUI ("PI", "Piauí"),
-	MARANHAO ("MA", "Maranhão");
+	MARANHAO ("MA", "Maranhão")
+	CEARA("CE","Ceara")
+	;
 	
 	private String nome;
 	private String sigla;
